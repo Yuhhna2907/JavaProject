850a1311bf79329c1f6ac875a866a07f274d3b73
Yuhhna2907 <luonganhhuy2004@gmail.com>
2025-10-17 14:32:09 +0700

    Add files via upload

---
+++ StudentAccessModifier/Text.java
@@ -0,0 +1,10 @@
+package StudentAccessModifier;
+
+public class Text {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Lương Anh Huy");
+        s1.setClasses("C0825G1");
+        s1.displayInfo();
+    }
+}

863bc89aa42e33ed8e6a97fbac15a661cb5741bd
Yuhhna2907 <luonganhhuy2004@gmail.com>
2025-10-28 14:24:20 +0700

    Add files via upload

---
+++ MyLinkedListQueue/Node.java
@@ -0,0 +1,11 @@
+package MyLinkedListQueue;
+
+public class Node {
+    public int key;
+    public String value;
+    public Node next;
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}

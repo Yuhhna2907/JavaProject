5093299895866b494c0c0fe7e377c572a430b69e
Yuhhna2907 <luonganhhuy2004@gmail.com>
2025-11-05 14:32:57 +0700

    Add files via upload

---
+++ CopyFileUsingStream/Main.java
@@ -0,0 +1,48 @@
+package CopyFileUsingStream;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter source file: ");
+        String sourcePath = in.nextLine();
+
+        System.out.print("Enter destination file: ");
+        String destPath = in.nextLine();
+
+        File sourceFile = new File(sourcePath);
+        File destFile = new File(destPath);
+
+        try {
+            // Thay đổi giữa hai phương thức để thấy sự khác biệt
+            copyFileUsingJava7Files(sourceFile, destFile);
+            // copyFileUsingStream(sourceFile, destFile);
+
+            System.out.println("Copy completed!");
+        } catch (IOException ioe) {
+            System.out.println("Can't copy that file");
+            System.out.println(ioe.getMessage());
+        }
+    }
+
+
+    private static void copyFileUsingJava7Files(File source, File dest) throws IOException {
+        Files.copy(source.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private static void copyFileUsingStream(File source, File dest) throws IOException {
+        try (InputStream is = new FileInputStream(source);
+             OutputStream os = new FileOutputStream(dest)) {
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        }
+    }
+}
